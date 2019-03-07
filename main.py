0ab8933cabd1c8fd5f6296621f51a67161b58df1
luiz henrique <luizhenrquefbb@gmail.com>
2019-03-07 08:26:24 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,162 @@
+
+import cv2  # openCV
+import argparse
+import numpy as np
+import timeit
+
+
+def convertArrayToNumpy(array):
+	'''
+	Coloca um array em forma de numpy_array pq eh o tipo de objeto que o openCV usa
+	'''
+	array_numPy=np.zeros((len(array), len(array[0]), 3))
+
+	for row in range(len(array)):
+		for colunm in range(len(array[row])):
+			array_numPy[row][colunm]=array[row][colunm]
+
+	return array_numPy
+
+def applyToAllPixels(img, action):
+	'''
+	Aplica uma uma funcao para todos os pixels
+	parametros:
+		img: array.numpy ou simplesmente um array 3-dimensional que contem os pixels
+		action: dicionario que contem duas chaves:
+			'fun': nome da funcao a ser aplicada
+			'parameters': parametros que a funcao usa alem do R,G,B
+			Ex.: Se a funcao a ser passada tiver seus proprios parametros, como 'programar(computador,cafe)',
+				os parametros computador e cafe estarao em outro dicionario separado (o dicionario seria o valor da
+				chave 'parameters'). A chamada da funcao do exemplo acima ficaria:
+					applyToAllPixels(img,{
+						'fun' : programar,
+						'parameters' : {
+							'maquina' : computador,
+							'liquido' : cafe
+							}
+						}
+					)
+	'''
+
+	height=len(img)
+	width=len(img[0])
+
+	fun=action.get('fun')
+	parameters=action.get('parameters')
+
+	# Usar metodo pixel a pixel
+	newImage=[]
+	for h in range(height):
+		newImage.append([])
+		for w in range(width):
+			# Verifica se precisa de parametros fora o R,G,B
+			if (parameters != None):
+				newImage[-1].append(fun(img[h][w][0], img[h][w][1],
+									img[h][w][2],  parameters))
+			else:
+				newImage[-1].append(fun(img[h][w][0], img[h][w][1],  img[h][w][2]))
+
+	return convertArrayToNumpy(newImage)
+
+def applyChange(originalImage, color='gray'):
+	'''
+	Coloca a imagem em modo monocromatico
+
+	Parametros:
+		originalImage: ----
+		color: string que siginfica a banda escolhida:
+			gray: --- (default)
+			red: ---
+			green:---
+			blue: ---
+	'''
+
+	imageResult = None
+
+	if (color == 'gray'):
+		# media dos  valores de cada pixel
+		imageResult = applyToAllPixels(originalImage, {'fun': calculateMeanAndAplly})
+
+	# para cores primarias basta remover os outros valores
+	elif (color == 'red'):
+		imageResult = applyToAllPixels(originalImage, {'fun': toRed})
+	elif (color == 'blue'):
+		imageResult = applyToAllPixels(originalImage, {'fun': toBlue})
+	elif (color == 'green'):
+		imageResult = applyToAllPixels(originalImage, {'fun': toGreen})
+	else:
+		print("invalid Color")
+
+	return imageResult
+
+def calculateMeanAndAplly(b,g,r):
+	'''
+	Calcula a media de tres pixels e retorna um pixel com essa media
+	'''
+	
+	average = np.mean([b,g,r])
+
+	return [average,average,average]
+
+def toGreen(b,g,r):
+	'''
+	converte todo o pixel para a banda verde
+	'''
+	return [0,g,0]
+
+def toRed (b,g,r):
+	'''
+	converte todo o pixel para a banda vermelha
+	'''
+	return [0,0,r]
+
+def toBlue (b,g,r):
+	'''
+	converte todo o pixel para a banda azul
+	'''
+	return [b,0,0]
+
+
+
+
+
+if __name__ == "__main__":
+	# construir o parse de argumentos
+	ap = argparse.ArgumentParser()
+
+	ap.add_argument("-i", "--image", required=False,
+					help="Path to the image to be scanned")
+
+	ap.add_argument("-c", "--scale", required=False,
+					help="Color of the scale. Can be 'gray', 'red', 'blue' or 'green'")
+
+	ap.add_argument("-o", "--output", required=True,
+					help="Path to the image to be saved")
+	
+	
+	args = vars(ap.parse_args())
+
+	# setar paramentos nao-obrigatorios
+	if args["image"] == None:
+		args["image"] = "image.jpeg"
+
+	if args["scale"] == None:
+		args["scale"] = "gray"
+
+
+	# ler imagem.
+	# A imagem lida eh um array[linha][coluna]
+	img = cv2.imread(args["image"])
+	
+	# aplicar mudancas
+
+	start = timeit.default_timer()
+
+	result = applyChange(img, args["scale"])
+
+	stop = timeit.default_timer()
+
+	print('Execution Time: ', stop - start)  
+
+	# salvar imagem
+	cv2.imwrite(args['output'],result)
---
+++ readme.md
@@ -0,0 +1,47 @@
+# Arquitetura II - Projeto I
+
+- Escolha um programa de código aberto na internet
+  - Analise seu código em busca de partes que não exploram corretamente o uso de memória cache
+  - Apresente as modificações e apresenta uma comparação no tempo de execução
+- Elabore um relatório técnico onde você apresenta
+  - Qual código utilizado (com URL, mas sem o código-fonte) 
+  - Quais pontos de melhoria foram encontrados
+  - Que mudanças você propôs
+  - Quais as melhorias em desempenho? (apresentar gráficos ou tabelas)
+- Se você não encontrar um código-fonte adequado, você deve implementar um código seu e apresentar uma comparação entre sua versão com - e sem otimização de memória.
+- Enviar o relatório por e-mail e apresentar a execução ao professor em sala de aula na data estabelecida
+
+# Sobre o projeto
+
+É um projeto simples que pega uma imagem de qualquer tamanho e deixa em uma escala monocromatica. Podendo ser cinza, vermelha, azul ou verde
+
+## Dependencias
+
+### Numpy
+```sh
+sudo apt-get install python-pip  
+sudo pip install numpy
+```
+
+### OPenCV
+```sh
+sudo apt-get install python-opencv
+```
+
+## Como executar
+
+```sh
+python3 main.py -i <caminho_da_imagem> -c <cor> -o <caminho_para_salvar_a_imagem>
+```
+
+Parametros
+
+    -i | --image   :   caminho_da_imagem original
+    -c | --scale   :   cor. Pode ser ["gray", "red", "blue", "green"]
+    -o | --output  :   caminho_para_salvar_a_imagem
+
+* Qualquer problema com alguma bilbioteca, tente instalar para o python3
+
+# Grupo
+- Aline Moura Araújo
+- Luiz Henrique Freire Barros
